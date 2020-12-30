c7de6e434190149402a59b2eb280755d88090f58
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2020-12-30 13:40:45 +0700

    :bug: fixed web socket :ambulance:

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -14,6 +14,8 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.action_cable.url = "wss://chat.syfa.dev/cable"
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
